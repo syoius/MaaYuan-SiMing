40000b643213cb1d530044001cced10f126de09b
Syoius <syoius@gmail.com>
2025-07-26 18:43:18 +0800

    chore: 作业信息改色

--- backend/fight_g.py
+++ backend/fight_g.py
@@ -292,7 +292,7 @@ def generate_config(input_path, output_path, level_type='', level_recognition_na
         }
 
         result_config["作业信息"] = {
-            "focus": "作业信息：由司命v1.9.3生成，需MaaYuan v0.9.13以上运行"
+            "focus": "[color:#D48806]作业信息：由司命v1.9.3生成，需MaaYuan v0.9.13以上运行[/color]"
         }
 
         result_config["抄作业胜利后继续"] = {
