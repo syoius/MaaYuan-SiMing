dc95e3875d2fcffd6b7e324120c493cad652c1b7
Snake <yangpeide0208@gmail.com>
2026-02-20 21:56:29 +0800

    fix: 修复抄作业无法正常重开

--- backend/services/config_generator.py
+++ backend/services/config_generator.py
@@ -406,7 +406,7 @@ class ConfigGenerator:
             "action": "Click",
             "pre_delay": 500,
             "post_delay": 2000,
-            "next": ["抄作业确定左上角重开", "抄作业退出兰台木桩", next_node],
+            "next": ["[JumpBack]抄作业确定左上角重开", "抄作业退出兰台木桩", next_node],
             "focus": "正在尝试点左上角重开",
             "timeout": 20000
         }
